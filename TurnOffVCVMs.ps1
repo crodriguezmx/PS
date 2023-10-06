ea4635f3e22a82054f7616adccc3c444cf346d5e
crodriguezmx <crodriguez.mx@gmail.com>
2023-10-05 21:37:56 -0600

    10/05/23 || 9:38PM

--- TurnOffVCVMs.ps1
+++ TurnOffVCVMs.ps1
@@ -13,22 +13,31 @@ Connect-VIServer -Server $vc -User $user -Password $pass
 $vms = Get-VM | Where-Object {$_.PowerState -eq 'PoweredOn'} 
 $vms | Select-Object Name, PowerState
 
+$response = Read-Host "¿Estas seguro de que quieres apagar todas las VMs? (Y/N)"
 
-#Shutdown all PowerOn VMs
-foreach ($vm in $vms) {
-    # No apagar la VM r1.labon.casa
-    if ($vm.Name -ne 'r1.labon.casa') {
-        Write-Host "Apagando la maquina virtual $($vm.Name)..."
-        Shutdown-VMGuest -VM $vm -Confirm:$false
-    } else {
-        Write-Host "Omitiendo la maquina virtual $($vm.Name)..."
+if ($response -eq 'Y' -or $response -eq 'y') {
+    Write-Host "Has confirmado. Continuando con la ejecución del apagado de VMs."
+    #Shutdown all PowerOn VMs
+    foreach ($vm in $vms) {
+        # No apagar la VM r1.labon.casa
+        if ($vm.Name -ne 'r1.labon.casa') {
+            Write-Host "Apagando la maquina virtual $($vm.Name)..."
+            Shutdown-VMGuest -VM $vm -Confirm:$false
+        } else {
+            Write-Host "Omitiendo la maquina virtual $($vm.Name)..."
+        }
     }
+    #PowerOff Host
+    $esxi2 = Get-VMHost -Name 'esx2.labon.casa'
+    $esxi3 = Get-VMHost -Name 'esx3.labon.casa'
+    
+    Write-Host "Apagando el host ESXi2..."
+    Stop-VMHost -VMHost $esxi2 -Confirm:$false -Force
+    Write-Host "Apagando el host ESXi3..."
+    Stop-VMHost -VMHost $esxi3 -Confirm:$false -Force
 }
 
-#PowerOff Host
-$esxi2 = Get-VMHost -Name 'esx2.labon.casa'
-$esxi3 = Get-VMHost -Name 'esx3.labon.casa'
-
-Write-Host "Apagando el host ESXi..."
-Stop-VMHost -VMHost $esxi2 -Confirm:$false -Force
-Stop-VMHost -VMHost $esxi3 -Confirm:$false -Force
+else {
+    Write-Host "Has decidido no continuar. Saliendo.."
+    exit
+}
